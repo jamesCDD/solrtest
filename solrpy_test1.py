3d6c8776f11a4cb3b345980801454e8a374d5bcb
jamesCDD <james_open2015@163.com>
2015-04-29 17:16:38 +0800

    更改s为conn

--- solrpy_test1.py
+++ solrpy_test1.py
@@ -6,7 +6,7 @@ sys.setdefaultencoding('utf-8')
 print sys.getdefaultencoding()
 
 # create a connection to a solr server
-s = solr.Solr('http://localhost:8080/solr', timeout=1000)
+conn = solr.Solr('http://localhost:8080/solr', timeout=1000)
 
 # add a document to the index
 tdoc = {"id": 3, "title": "Lucene in Action"}
@@ -14,11 +14,11 @@ tdoc = {"id": 3, "title": "Lucene in Action"}
 for k in tdoc:
     print "dict[%s] =" % k, tdoc[k]
 
-s.add(tdoc)
-s.commit()
+conn.add(tdoc)
+conn.commit()
 
 # do a search
-response = s.select('Lucene')
+response = conn.select('Lucene')
 for hit in response.results:
 
     print hit
